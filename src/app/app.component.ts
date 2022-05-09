d9d6571fc44fa7035c710d42b7174f1148704b9d
Mark Thompson <2554588+MarkTechson@users.noreply.github.com>
2022-05-09 13:18:58 -0700

    adds updatedf UI for translations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'i18n-receipt-demo';
+  title = 'Your Receipt';
+
+  constructor(private titleService: Title) {
+    this.titleService.setTitle(this.title);
+  }
+
 }
